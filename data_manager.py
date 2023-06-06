94dd3388be948102e6a36fd27ee5fdc74f57496d
Ilona Drzymała <idrzym@gmail.com>
2023-06-06 19:32:20 +0200

    connection, updated read, write, update

---
+++ data_manager.py
@@ -0,0 +1,3 @@
+import connection
+import server
+
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
+import flask
 from flask import Flask
+import connection
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def main_page():
+    return flask.render_template("main.html")
+
+
+@app.route('/list')
+def list_questions(file="question.csv"):
+    user_questions = connection.read_dict_from_file(file)
+    return flask.render_template("list.html", questions=user_questions)
 
 
 if __name__ == "__main__":
