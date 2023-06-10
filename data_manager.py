7e878a475b2ed384e4eb5da8c0d50d25d643afb0
Ilona Drzymała <idrzym@gmail.com>
2023-06-10 16:58:59 +0200

    editing questions, generating names for files, fixed bug with deleting question, seems like the project is DONE ;)

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import connection
 import util
+import os
 
 questions_csv = "data/question.csv"
 answers_csv = "data/answer.csv"
@@ -28,7 +29,7 @@ def get_answers_by_question_id_dm(question_id):
     return answers
 
 
-def add_question_dm(title, message, question_id=0):
+def add_question_dm(title, message, image_path=None, question_id=0):
     questions = connection.read_dict_from_file(questions_csv)
     question_id = str(util.generated_id(questions_csv)) if question_id == 0 else question_id
     new_question = {
@@ -38,7 +39,7 @@ def add_question_dm(title, message, question_id=0):
         "vote_number": 0,
         'title': title,
         'message': message,
-        "image": 0
+        "image": image_path
 
     }
     questions.append(new_question)
@@ -46,7 +47,7 @@ def add_question_dm(title, message, question_id=0):
     return question_id
 
 
-def add_answer_dm(message, question_id):
+def add_answer_dm(message, question_id, image_path=None):
     answers = connection.read_dict_from_file(answers_csv)
     new_answer_id = str(util.generated_id(answers_csv))
     new_answer = {
@@ -55,7 +56,7 @@ def add_answer_dm(message, question_id):
         "vote_number": 0,
         "question_id": question_id,
         'message': message,
-        "image": 0
+        "image": image_path
     }
     answers.append(new_answer)
     connection.write_dict_to_file_str(answers_csv, answers, HEADERS_A)
@@ -63,8 +64,13 @@ def add_answer_dm(message, question_id):
 
 def get_sorted_questions(order_by, order_direction):
     questions = connection.read_dict_from_file(questions_csv)
+    answers = connection.read_dict_from_file(answers_csv)
     for question in questions:
         question["submission_time"] = util.convert_timestamp_to_date(int(question["submission_time"]))
+        question['answers'] = 0
+        for answer in answers:
+            if question['id'] == answer['question_id']:
+                question['answers'] += 1
     if order_by == 'title':
         questions.sort(key=lambda q: q['title'].lower(), reverse=(order_direction == 'desc'))
     elif order_by == 'submission_time':
@@ -75,19 +81,31 @@ def get_sorted_questions(order_by, order_direction):
         questions.sort(key=lambda q: int(q['view_number']), reverse=(order_direction == 'desc'))
     elif order_by == 'vote_number':
         questions.sort(key=lambda q: int(q['vote_number']), reverse=(order_direction == 'desc'))
+    elif order_by == 'answers':
+        questions.sort(key=lambda q: int(q['answers']), reverse=(order_direction == 'desc'))
     return questions
 
 
-def delete_answer(data_id, data_type):
+def delete_answer_dm(data_id, data_type):
+    header = 'question_id' if data_type == "question" else 'id'
     answers = connection.read_dict_from_file(answers_csv)
-    answers = [answer for answer in answers if answer.get('question_id'
-                                                          if data_type == "question" else 'id') != data_id]
+    for answer in answers:
+        file_path = answer['image']
+        if answer.get(header) == data_id:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            answers.remove(answer)
     connection.write_dict_to_file_str(answers_csv, answers, HEADERS_A)
 
 
-def delete_question(question_id):
+def delete_question_dm(question_id):
     questions = connection.read_dict_from_file(questions_csv)
-    questions = [question for question in questions if question.get('id') != question_id]
+    for question in questions:
+        file_path = question['image']
+        if question.get('id') == question_id:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            questions.remove(question)
     connection.write_dict_to_file_str(questions_csv, questions, HEADERS_Q)
 
 
@@ -98,12 +116,12 @@ def get_question_id(answer_id):
             return answer['question_id']
 
 
-def update_question(title, message, question_id):
-    delete_question(question_id)
-    add_question_dm(title, message, question_id)
+def update_question_dm(title, message, image_path, question_id):
+    delete_question_dm(question_id)
+    add_question_dm(title, message, image_path, question_id)
 
 
-def vote_dm(data_id, data_type, vote):
+def vote_on_dm(data_id, data_type, vote):
     data_list = connection.read_dict_from_file(questions_csv if data_type == "q" else answers_csv)
     for data in data_list:
         if data['id'] == data_id:
@@ -127,3 +145,4 @@ def view_question_dm(question_id):
             question["view_number"] = number_of_views
 
     connection.write_dict_to_file_str(questions_csv, questions, HEADERS_Q)
+
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import flask
 from flask import Flask
-
 import data_manager
+import os
+import uuid
 
 app = Flask(__name__)
 
@@ -27,8 +28,8 @@ def print_question(question_id):
     data_manager.view_question_dm(question_id)
     question = data_manager.get_question_data_by_id_dm(question_id)
     answers = data_manager.get_answers_by_question_id_dm(question_id)
-    return flask.render_template('question.html', question=question[TITLE],
-                                 message=question[MESSAGE], answers=answers, question_id=question_id)
+    return flask.render_template('question.html', question=question[TITLE], message=question[MESSAGE],
+                                 answers=answers, question_id=question_id, image=question[IMAGE])
 
 
 @app.route('/add_question', methods=['GET', 'POST'])
@@ -36,7 +37,13 @@ def add_question():
     if flask.request.method == 'POST':
         title = flask.request.form['title']
         message = flask.request.form['message']
-        new_question_id = data_manager.add_question_dm(title, message)
+        image_file = flask.request.files['image']
+        image_path = None
+        if 'image' in flask.request.files and image_file.filename != '':
+            unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+            image_path = 'static/uploads/' + unique_filename
+            image_file.save(image_path)
+        new_question_id = data_manager.add_question_dm(title, message, image_path)
         return flask.redirect(f'/question/{new_question_id}')
     else:
         return flask.render_template('add_question.html', version="edit")
@@ -46,7 +53,13 @@ def add_question():
 def add_answer(question_id):
     if flask.request.method == 'POST':
         message = flask.request.form['message']
-        data_manager.add_answer_dm(message, question_id)
+        image_file = flask.request.files['image']
+        image_path = None
+        if 'image' in flask.request.files and image_file.filename != '':
+            unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+            image_path = 'static/uploads/' + unique_filename
+            image_file.save(image_path)
+        data_manager.add_answer_dm(message, question_id, image_path)
         return flask.redirect(f'/question/{question_id}')
     elif flask.request.method == 'GET':
         return flask.render_template('add_answer.html', question_id=question_id)
@@ -54,15 +67,15 @@ def add_answer(question_id):
 
 @app.route('/question/<question_id>/delete')
 def delete_question(question_id):
-    data_manager.delete_question(question_id)
-    data_manager.delete_answer(question_id, "question")
+    data_manager.delete_question_dm(question_id)
+    data_manager.delete_answer_dm(question_id, "question")
     return flask.redirect('/list')
 
 
 @app.route('/answer/<answer_id>/delete')
 def delete_answer(answer_id):
     question_id = data_manager.get_question_id(answer_id)
-    data_manager.delete_answer(answer_id, "answer")
+    data_manager.delete_answer_dm(answer_id, "answer")
     return flask.redirect(f'/question/{question_id}')
 
 
@@ -71,37 +84,45 @@ def edit_question(question_id):
     if flask.request.method == 'GET':
         question = data_manager.get_question_data_by_id_dm(question_id)
         return flask.render_template('edit_question.html', message=question[MESSAGE], question_id=question_id,
-                                     title=question[TITLE])
-    if flask.request.method == 'POST':
+                                     title=question[TITLE], image=question[IMAGE])
+    elif flask.request.method == 'POST':
         title = flask.request.form['title']
         message = flask.request.form['message']
-        data_manager.update_question(title, message, question_id)
+        image_file = flask.request.files['image']
+        if 'image' in flask.request.files and image_file.filename != '':
+            unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+            image_path = 'static/uploads/' + unique_filename
+            image_file.save(image_path)
+        else:
+            question = data_manager.get_question_data_by_id_dm(question_id)
+            image_path = question[IMAGE]
+        data_manager.update_question_dm(title, message, image_path, question_id)
         return flask.redirect(f'/question/{question_id}')
 
 
 @app.route('/question/<question_id>/vote_up')
 def vote_up_questions(question_id):
-    data_manager.vote_dm(question_id, "q", "up")
+    data_manager.vote_on_dm(question_id, "q", "up")
     return flask.redirect('/list')
 
 
 @app.route('/question/<question_id>/vote_down')
 def vote_down_questions(question_id):
-    data_manager.vote_dm(question_id, "q", "down")
+    data_manager.vote_on_dm(question_id, "q", "down")
     return flask.redirect('/list')
 
 
 @app.route('/answer/<answer_id>/vote_up')
 def vote_up_answers(answer_id):
     question_id = data_manager.get_question_id(answer_id)
-    data_manager.vote_dm(answer_id, "a", "up")
+    data_manager.vote_on_dm(answer_id, "a", "up")
     return flask.redirect(f'/question/{question_id}')
 
 
 @app.route('/answer/<answer_id>/vote_down')
 def vote_down_answers(answer_id):
     question_id = data_manager.get_question_id(answer_id)
-    data_manager.vote_dm(answer_id, "a", "down")
+    data_manager.vote_on_dm(answer_id, "a", "down")
     return flask.redirect(f'/question/{question_id}')
 
 
